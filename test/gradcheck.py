Fixed gradients for locs and qlocs in ConvSP so they are correct and enabled requires_grad for locs in ConvSDF. Also removed the bidirectional argument to compute_kernel_cells in common_funcs.h because it didn't have a performance boost but added a lot of conceptual overhead. Finally, removed viscosity kernels as they caused too much numerical instability.

test/gradcheck.py
@@ -64,16 +64,19 @@ def contiguous(input):
     return input
 
 
-def get_numerical_jacobian(fn, input, target, eps=1e-3):
+def get_numerical_jacobian(fn, input, eps=1e-3, use_double=False):
     # To be able to use .view(-1) input must be contiguous
     input = contiguous(input)
+    if use_double:
+        input = [torch.nn.Parameter(x.data.double()) if isinstance(x, torch.nn.Parameter) else x.double() 
+                    for x in input]
     output_size = fn(input).numel()
-    jacobian = make_jacobian(target, output_size)
+    jacobian = make_jacobian(input, output_size)
 
     # It's much easier to iterate over flattened lists of tensors.
     # These are reference to the same objects in jacobian, so any changes
     # will be reflected in it as well.
-    x_tensors = [t for t in iter_tensors(target, True)]
+    x_tensors = [t for t in iter_tensors(input, True)]
     j_tensors = [t for t in iter_tensors(jacobian)]
 
     outa = torch.DoubleTensor(output_size)
@@ -93,7 +96,7 @@ def get_numerical_jacobian(fn, input, target, eps=1e-3):
             outb.add_(-1, outa).div_(2 * eps)
             d_tensor[i] = outb
 
-    return jacobian
+    return tuple(x.float() for x in jacobian)
 
 
 def get_analytical_jacobian(input, output):
@@ -137,7 +140,8 @@ def _differentiable_outputs(x):
     return tuple(o for o in _as_tuple(x) if o.requires_grad)
 
 
-def gradcheck(func, inputs, eps=1e-6, atol=1e-5, rtol=1e-3, retol=1e-4, raise_exception=True):
+def gradcheck(func, inputs, eps=1e-6, atol=1e-5, rtol=1e-3, retol=1e-4, raise_exception=True,
+    func_numerical=None, use_double=False):
     """Check gradients computed via small finite differences
        against analytical gradients
     The check between numerical and analytical has the same behaviour as
@@ -156,6 +160,10 @@ def gradcheck(func, inputs, eps=1e-6, atol=1e-5, rtol=1e-3, retol=1e-4, raise_ex
         raise_exception: bool indicating whether to raise an exception if
             gradcheck fails. The exception gives more information about the
             exact nature of the failure. This is helpful when debugging gradchecks.
+        func_numerical: Python function identical to func but to be used when doing
+                        numerical derivatives.
+        use_double: If true, will pass double tensors to func when computing numerical
+                    grads.
     Returns:
         True if all differences satisfy allclose condition
     """
@@ -170,11 +178,13 @@ def gradcheck(func, inputs, eps=1e-6, atol=1e-5, rtol=1e-3, retol=1e-4, raise_ex
         if not o.requires_grad:
             continue
 
+        if func_numerical is None:
+            func_numerical = func
         def fn(input):
-            return _as_tuple(func(*input))[i].data
+            return _as_tuple(func_numerical(*input))[i].data
 
         analytical, reentrant, correct_grad_sizes = get_analytical_jacobian(_as_tuple(inputs), o)
-        numerical = get_numerical_jacobian(fn, inputs, inputs, eps)
+        numerical = get_numerical_jacobian(fn, inputs, eps, use_double)
 
         if reentrant > retol:
             return fail_test('not reentrant, %g exceeded reentrance tolerance of %g.' 

test/test_convsdf.py
@@ -150,7 +150,7 @@ def eval_convsdf(cuda=False):
     sdfs_t = [torch.FloatTensor(x) for x in sdfs]
     sdf_sizes_t = [np.mean([1.0*sdf_widths[i, j]/sdfs[i].shape[j] 
                     for j in range(len(sdfs[i].shape))]) for i in range(len(sdfs))] 
-    locs_t = torch.autograd.Variable(use_cuda(torch.FloatTensor(locs)), requires_grad=False)
+    locs_t = torch.autograd.Variable(use_cuda(torch.FloatTensor(locs)), requires_grad=True)
     idxs_t = torch.autograd.Variable(use_cuda(torch.FloatTensor(idxs)), requires_grad=False)
     poses_t = torch.autograd.Variable(use_cuda(torch.FloatTensor(sdf_poses)), 
                                         requires_grad=False)

test/test_convsp.py
@@ -16,9 +16,36 @@ except ImportError:
     print("Make sure to compile SmoothParticleNets before running tests.")
     raise
 
-# The kernel function for SPH.
-def w(x, h=1):
-    return 1.0/np.pi*(0.25*max(0, h - x)**3 - max(0, h/2.0 - x)**3)/(h**3/(8*np.pi))
+
+def pyconvsp(qlocs, locs, data, weights, biases, kernel_fn, KERNEL_SIZE, RADIUS, DILATION, NKERNELS):
+    w = eval("lambda d, H: " + spn.KERNELS[kernel_fn]
+            .replace("M_PI", "np.pi")
+            .replace("fmaxf", "max")
+            .replace("f", ""))
+
+    BATCH_SIZE = locs.shape[0]
+    M = qlocs.shape[1]
+    N = locs.shape[1]
+    NDIM = locs.shape[-1]
+
+    kernel_centers = (np.array(KERNEL_SIZE) - 1)/2
+    ground_truth = np.zeros((BATCH_SIZE, M, NKERNELS), dtype=data.dtype)
+    for b in range(BATCH_SIZE):
+        for i in range(M):
+            for j in range(N):
+                dd = np.square(qlocs[b, i, :] - locs[b, j, :]).sum()
+                nr = DILATION*max(KERNEL_SIZE)/2 + RADIUS
+                if dd > nr*nr:
+                    continue
+                for k, idxs in enumerate(itertools.product(*[range(x) for x in KERNEL_SIZE[::-1]])):
+                    dd = np.square(qlocs[b, i, :] + (idxs[::-1] - kernel_centers)*DILATION 
+                        - locs[b, j, :]).sum()
+                    if dd > RADIUS*RADIUS:
+                        continue
+                    ground_truth[b, i, :] += weights[:, :, k].dot(
+                        w(np.sqrt(dd), RADIUS)*data[b, j, :])
+    ground_truth += biases[np.newaxis, np.newaxis, :]
+    return ground_truth
 
 
 def test_convsp(cpu=True, cuda=True):
@@ -38,41 +65,30 @@ def test_convsp(cpu=True, cuda=True):
 
 def eval_convsp(cuda=False):
     BATCH_SIZE = 2
-    N = 10
-    M = 13
+    N = 5
+    M = 3
     NDIM = 2
-    KERNEL_SIZE = (3, 5)
+    KERNEL_SIZE = (3, 1)
     RADIUS = 1.0
     DILATION = 0.05
     NCHANNELS = 2
     NKERNELS = 3
-    MASS = 1.0
+
+    BATCH_SIZE = 1
+    N = 2
+    M = 2
+    NDIM = 1
+    KERNEL_SIZE = (1,)
+    NCHANNELS = 1
+    NKERNELS = 1
 
     np.random.seed(0)
 
-    locs = np.random.rand(BATCH_SIZE, N, NDIM)
-    qlocs = np.random.rand(BATCH_SIZE, M, NDIM)
-    data = np.random.rand(BATCH_SIZE, N, NCHANNELS)
-    weights = np.random.rand(NKERNELS, NCHANNELS, np.prod(KERNEL_SIZE))
-    biases = np.random.rand(NKERNELS)
-
-    kernel_centers = (np.array(KERNEL_SIZE) - 1)/2
-    ground_truth = np.zeros((BATCH_SIZE, M, NKERNELS), dtype=np.float32)
-    for b in range(BATCH_SIZE):
-        for i in range(M):
-            for j in range(N):
-                d = np.square(qlocs[b, i, :] - locs[b, j, :]).sum()
-                nr = DILATION*max(KERNEL_SIZE)/2 + RADIUS
-                if d > nr*nr:
-                    continue
-                for k, idxs in enumerate(itertools.product(*[range(x) for x in KERNEL_SIZE[::-1]])):
-                    d = np.square(qlocs[b, i, :] + (idxs[::-1] - kernel_centers)*DILATION 
-                        - locs[b, j, :]).sum()
-                    if d > RADIUS*RADIUS:
-                        continue
-                    ground_truth[b, i, :] += weights[:, :, k].dot(
-                        w(np.sqrt(d), h=RADIUS)*data[b, j, :])
-    ground_truth += biases[np.newaxis, np.newaxis, :]
+    locs = np.random.rand(BATCH_SIZE, N, NDIM).astype(np.float32)
+    qlocs = np.random.rand(BATCH_SIZE, M, NDIM).astype(np.float32)
+    data = np.random.rand(BATCH_SIZE, N, NCHANNELS).astype(np.float32)
+    weights = np.random.rand(NKERNELS, NCHANNELS, np.prod(KERNEL_SIZE)).astype(np.float32)
+    biases = np.random.rand(NKERNELS).astype(np.float32)
 
     def use_cuda(x):
         if cuda:
@@ -85,64 +101,56 @@ def eval_convsp(cuda=False):
         else:
             return x
 
-    locs = torch.autograd.Variable(use_cuda(torch.FloatTensor(locs)), requires_grad=False)
-    qlocs = torch.autograd.Variable(use_cuda(torch.FloatTensor(qlocs)), requires_grad=False)
-    data = torch.autograd.Variable(use_cuda(torch.FloatTensor(data)), requires_grad=True)
-    weights = torch.nn.Parameter(torch.FloatTensor(weights), requires_grad=True)
-    biases = torch.nn.Parameter(torch.FloatTensor(biases), requires_grad=True)
+    for use_qlocs in (True, False):
 
-    coll = use_cuda(spn.ParticleCollision(NDIM, 
-        RADIUS + DILATION*max((k - 1)/2 for k in KERNEL_SIZE)))
-    locs, data, idxs, neighbors = coll(locs, data, qlocs)
-    # reorder = use_cuda(spn.ReorderData(reverse=False))
-    # ground_truth = torch.autograd.Variable(use_cuda(torch.FloatTensor(ground_truth)), 
-    #     requires_grad=False)
-    # reorder(idxs, ground_truth)
-    # ground_truth = undo_cuda(ground_truth).data.numpy()
+        locs_t = torch.autograd.Variable(use_cuda(torch.FloatTensor(locs)), requires_grad=True)
+        if use_qlocs:
+            qlocs_t = torch.autograd.Variable(use_cuda(torch.FloatTensor(qlocs)), requires_grad=True)
+        else:
+            qlocs_t = None
+        data_t = torch.autograd.Variable(use_cuda(torch.FloatTensor(data)), requires_grad=True)
+        weights_t = torch.nn.Parameter(torch.FloatTensor(weights), requires_grad=True)
+        biases_t = torch.nn.Parameter(torch.FloatTensor(biases), requires_grad=True)
 
-    convsp = spn.ConvSP(NCHANNELS, NKERNELS, NDIM, KERNEL_SIZE, DILATION, RADIUS)
-    convsp.weight = weights
-    convsp.bias = biases
-    convsp = use_cuda(convsp)
-    if cuda:
-        # Set convsp's amount of shared memory low enough so that the convsp cuda
-        # implementation has to split the particles into blocks.
-        device_id = torch.cuda.current_device()
-        nshared_device_mem = spn._ext.spnc_get_shared_mem_size(device_id)
-        f32 = np.dtype('float32').itemsize
-        fixedmem = (NKERNELS*NCHANNELS*np.prod(KERNEL_SIZE)*f32)
-        fixedmem += NDIM*f32
-        fixedmem += NDIM*f32
-        memperparticle = NDIM*f32
-        memperparticle += NCHANNELS*f32
-        memperparticle += f32
-        memperparticle += NKERNELS*f32
-        block_size = N//3
-        convsp.device_id = device_id
-        convsp.nshared_device_mem = min(nshared_device_mem, 
-            fixedmem + block_size*memperparticle*2)
+        coll = use_cuda(spn.ParticleCollision(NDIM, 
+            RADIUS + DILATION*max((k - 1)/2 for k in KERNEL_SIZE)))
+        locs_t, data_t, idxs_t, neighbors_t = coll(locs_t, data_t, (qlocs_t if use_qlocs else None))
 
+        for kernel_fn in spn.KERNEL_NAMES:
+            print("\tTesting kernel %s (%s query locations)..." % 
+                (kernel_fn, "with" if use_qlocs else "without"))
+            ground_truth = pyconvsp((qlocs if use_qlocs else locs), locs, data, weights, biases, 
+                kernel_fn, KERNEL_SIZE, RADIUS, DILATION, NKERNELS)
 
-    pred = undo_cuda(convsp(locs, data, neighbors, qlocs))
-    np.testing.assert_array_almost_equal(pred.data.numpy(), ground_truth, decimal=3)
+            convsp = spn.ConvSP(NCHANNELS, NKERNELS, NDIM, KERNEL_SIZE, DILATION, RADIUS,
+                kernel_fn=kernel_fn)
+            convsp.weight = weights_t
+            convsp.bias = biases_t
+            convsp = use_cuda(convsp)
 
-    if cuda:
-        # Add more device memory for doing backwards passes.
-        fixedmem += (NKERNELS*NCHANNELS*np.prod(KERNEL_SIZE)*f32)
-        memperparticle += NCHANNELS*f32
-        block_size = N//3
-        convsp.nshared_device_mem = min(nshared_device_mem, 
-            fixedmem + block_size*memperparticle*2)
+            pred_t = undo_cuda(convsp(locs_t, data_t, neighbors_t, qlocs_t))
+            np.testing.assert_array_almost_equal(pred_t.data.numpy(), ground_truth, decimal=3)
 
-
-    neighbors = torch.autograd.Variable(neighbors.data, requires_grad=False)
-    data = torch.autograd.Variable(data.data, requires_grad=True)
-    locs = torch.autograd.Variable(locs.data, requires_grad=False)
-    def func(l, d, w, b, q):
-        convsp.weight = w
-        convsp.bias = b
-        return (convsp(l, d, neighbors, q),)
-    assert gradcheck(func, (locs, data, weights, biases, qlocs), eps=1e-2, atol=1e-3)
+            dt = torch.autograd.Variable(data_t.data, requires_grad=True)
+            lt = torch.autograd.Variable(locs_t.data, requires_grad=True)
+            if use_qlocs:
+                qt = torch.autograd.Variable(qlocs_t.data, requires_grad=True)
+            wt = torch.nn.Parameter(weights_t.data, requires_grad=True)
+            bt = torch.nn.Parameter(biases_t.data, requires_grad=True)
+            # Use pyconvsp to allow for double precision when computing numeric grads.
+            def func_numerical(l, d, w, b, q=None):
+                return (torch.autograd.Variable(torch.from_numpy(
+                    pyconvsp((q.data.cpu().numpy() if use_qlocs else l.data.cpu().numpy()), 
+                        l.data.cpu().numpy(), 
+                        d.data.cpu().numpy(), w.data.cpu().numpy(), b.data.cpu().numpy(), 
+                        kernel_fn, KERNEL_SIZE, RADIUS, DILATION, NKERNELS))),)
+            def func_analytical(l, d, w, b, q=None):
+                convsp.weight = w
+                convsp.bias = b
+                return (convsp(l, d, neighbors_t, (q if use_qlocs else None)),)
+            assert gradcheck(func_analytical, 
+                ((lt, dt, wt, bt, qt) if use_qlocs else (lt, dt, wt, bt,)), 
+                eps=1e-4, atol=1e-3, rtol=1e-2, func_numerical=func_numerical, use_double=True)
 
 
 